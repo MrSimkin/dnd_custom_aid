fdf574b77f70f0e550a2714990fe3e036d448d6c
MrSimkin <gustavo.munoz.s@outlook.com>
2026-08-30 22:47:24 -0400

    Wire downloadable typography support

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.compose.material3)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
