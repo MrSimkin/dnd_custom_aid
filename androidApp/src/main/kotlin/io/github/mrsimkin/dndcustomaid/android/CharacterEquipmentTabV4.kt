a4ffd4d4395fb03817e295ac74fe983b9ba7f212
MrSimkin <gustavo.munoz.s@outlook.com>
2026-08-31 16:08:45 -0400

    Fix Equipment tab keyboard import

--- androidApp/src/main/kotlin/io/github/mrsimkin/dndcustomaid/android/CharacterEquipmentTabV4.kt
+++ androidApp/src/main/kotlin/io/github/mrsimkin/dndcustomaid/android/CharacterEquipmentTabV4.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
@@ -33,7 +34,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import io.github.mrsimkin.dndcustomaid.shared.character.CharacterCurrency
