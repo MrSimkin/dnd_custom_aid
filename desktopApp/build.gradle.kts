ed86b2d45b77a32e5eafc68a99cd583aac7138f4
MrSimkin <gustavo.munoz.s@outlook.com>
2026-09-17 22:33:41 -0300

    build: add Desktop PDF template rendering support

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -6,6 +6,12 @@ plugins {
 
 kotlin {
     jvmToolchain(17)
+
+    sourceSets {
+        main {
+            resources.srcDir(rootProject.file("assets"))
+        }
+    }
 }
 
 dependencies {
@@ -14,6 +20,7 @@ dependencies {
     implementation(libs.compose.multiplatform.material)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.ktor.client.core)
+    implementation(libs.pdfbox)
     testImplementation(kotlin("test"))
 }
 
